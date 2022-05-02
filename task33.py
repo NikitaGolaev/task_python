e1cbb4ec7093f13404d034f739b2f64da486c299
Elvira <elviraw@mail.ru>
2022-05-02 14:02:52 +0300

    add Annaeva2.txt and changed task33 for task34

--- task33.py
+++ task33.py
@@ -5,21 +5,28 @@ from pydoc import replace
 import random
 k=2
 lst = list()
+lst_1=list()
 def rand():                     #метод создания рандомного числа
     r = random.randint(0, 101) 
     return r
-def create_list():              #метод создания списка случайных коэффициентов
+def create_list(lst):              #метод создания списка случайных коэффициентов
     values = 3
     while values > 0:
         lst.append(rand())
         values= values - 1
     return lst
-def polynominal():              # метод создания строки многочлена
+def polynominal(lst):              # метод создания строки многочлена
     n = lst[0], '*x^2 +', lst[1],'*x + ',lst[2], '=', 0
     n = str(n)
     n_str = n.replace(",","").replace("'","").replace("(","").replace(")","")
     return n_str
-create_list()
-print(polynominal())
+create_list(lst)
+create_list(lst_1)
+print(polynominal(lst))
 with open('Annaeva.txt', 'w') as data:
-   data.write(polynominal())
+   data.write(polynominal(lst))
+# для task34
+
+with open('Annaeva2.txt', 'w') as data:
+   data.write(polynominal(lst_1))
+print(polynominal(lst_1))
